98eb71262304e3e69bd6542dc50aff2decc6a4b7
pratyush <pratyushpandey3897@gmail.com>
2022-08-21 18:02:54 -0700

    dsa code practice 08/21

---
+++ src/main/java/MaxSubarraySum.java
@@ -0,0 +1,50 @@
+import java.util.*;
+class MaxSubarraySum {
+    public static void main(String[] args) {
+
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        int n = nums.length;
+        System.out.println(maxSubArraySum(nums, 0, n-1));
+
+    }
+
+    public static int maxSubArraySum(int[] arr, int low, int high) {
+
+        if (low == high){
+            return arr[high];
+        }
+
+        int mid = (low + high)/2;
+
+        return Math.max(maxCrossinSum(arr, low, mid, high),
+                Math.max(maxSubArraySum(arr, low, mid),
+                        maxSubArraySum(arr, mid+1, high)));
+
+
+
+    }
+
+    public static int maxCrossinSum(int[] arr, int low, int mid, int high) {
+
+        int sum = 0;
+        int left_sum = Integer.MIN_VALUE;
+        for (int i = mid; i >= low; i--) {
+            sum = sum + arr[i];
+            if (sum > left_sum)
+                left_sum = sum;
+        }
+
+        sum = 0;
+        int right_sum = Integer.MIN_VALUE;
+        for (int i = mid+1; i <= high; i++) {
+            sum = sum + arr[i];
+            if (sum > right_sum)
+                right_sum = sum;
+        }
+
+        int crossingSum = left_sum + right_sum;
+
+        return Math.max(Math.max(left_sum, right_sum), crossingSum);
+
+    }
+}
--- src/main/java/PascalsTriangle.java
+++ src/main/java/PascalsTriangle.java
@@ -2,30 +2,30 @@ import java.util.*;
 class Solution {
     public static List<List<Integer>> generate(int numRows) {
         List<List<Integer>> list = new ArrayList<>();
-        
+
         if(numRows<=0)
         {
             return list;
         }
-        
+
         for(int line = 1; line <= numRows; line++)
-    {
-          
+        {
+
             List<Integer> l1 = new ArrayList<>();
-    int C=1;// used to represent C(line, i)
-            
-    for(int i = 1; i <= line; i++)
-    { 
-        l1.add(C);
-        // The first value in a line is always 1
-        C = C * (line - i) / i;
-        
-    }
+            int C=1;// used to represent C(line, i)
+
+            for(int i = 1; i <= line; i++)
+            {
+                l1.add(C);
+                // The first value in a line is always 1
+                C = C * (line - i) / i;
+
+            }
             list.add(l1);
-    }
-       
-    return list;
-    
+        }
+
+        return list;
+
     }
 
     public static void main(String[] args){
